538c8bf95adce2bfadccc79fba2fa485b44d0cf8
michal.sobolewski <michal.sobolewski@5dc8b5b5-2d95-451b-a4db-33bd0ba970db>
2013-02-27 08:44:11 +0000

    Initial import.
    git-svn-id: https://svn.smtsoftware.com/sorcer/trunk@15 5dc8b5b5-2d95-451b-a4db-33bd0ba970db

---
+++ iGrid/bin/rio/lincoln/bin/lincoln.bat
@@ -0,0 +1,44 @@
+@echo off
+
+@rem /*
+@rem 
+@rem Copyright 2005 Sun Microsystems, Inc.
+@rem Copyright 2005 GigaSpaces, Inc.
+@rem 
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem 
+@rem 	http://www.apache.org/licenses/LICENSE-2.0
+@rem 
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem 
+@rem */
+
+rem This script starts a non-persistent Lincoln using the start-lincoln.config configuration file found in
+rem %RIO_UTILS%\configs. 
+
+if exist envcheck.bat call envcheck.bat
+
+if errorlevel 1 goto end
+
+if "%RIO_HOME" == "" goto homeEnvFailure
+
+setlocal
+
+set classpath=-cp %RIO_HOME%\lib\boot.jar;%JINI_HOME%\lib\start.jar;
+set launchTarget=com.sun.jini.start.ServiceStarter
+
+"%JAVA_HOME%\bin\java" -server %classpath% -Djava.security.policy=%RIO_UTILS%\policy\policy.all -Djava.protocol.handler.pkgs=net.jini.url -DRIO_UTILS=%RIO_UTILS% -DRIO_HOME=%RIO_HOME% -DJINI_HOME=%JINI_HOME% %launchTarget% %RIO_UTILS%\configs\start-lincoln.config 
+
+endlocal
+
+:homeEnvFailure
+echo "Set the RIO_HOME environment variable to point to the location where you have installed Rio 
+
+:end
+
---
+++ iGrid/bin/rio/monitor/bin/activatable-monitor.bat
@@ -0,0 +1,39 @@
+@echo off
+
+@rem /*
+@rem 
+@rem Copyright 2005 Sun Microsystems, Inc.
+@rem 
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem 
+@rem 	http://www.apache.org/licenses/LICENSE-2.0
+@rem 
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem 
+@rem */
+
+rem This script starts a persistent and activatable Provision Monitor using the start-activatable-monitor.config
+rem configuration file found in %RIO_HOME%\configs.
+
+if exist envcheck.bat call envcheck.bat
+
+if errorlevel 1 goto end
+
+setlocal
+
+set RIO_LOG_DIR="%USERPROFILE%"\.rio\logs\
+
+set classpath=-cp %RIO_HOME%\lib\boot.jar;%JINI_HOME%\lib\start.jar;
+set launchTarget=com.sun.jini.start.ServiceStarter
+
+%JAVA_HOME%\bin\java -server -jar -Djava.security.policy=%RIO_HOME%\policy\policy.all -Djava.protocol.handler.pkgs=net.jini.url -DRIO_HOME=%RIO_HOME% -DJINI_HOME=%JINI_HOME% -DRIO_LOG_DIR=%RIO_LOG_DIR% %RIO_HOME%\lib\boot.jar %IGRID_HOME%\bin\rio\monitor\start-activatable-monitor.config
+
+endlocal
+
+:end
