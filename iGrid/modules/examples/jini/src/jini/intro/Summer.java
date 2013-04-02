edc11578d9b34fb9bc956fb5159aebd47f96caaf
pawel.rubach <pawel.rubach@5dc8b5b5-2d95-451b-a4db-33bd0ba970db>
2013-04-02 20:22:35 +0000

    Fixed requestors in examples: Arithmetic, deleted unused examples
    git-svn-id: https://svn.smtsoftware.com/sorcer/branches/maven@160 5dc8b5b5-2d95-451b-a4db-33bd0ba970db

--- iGrid/modules/examples/jini/src/jini/intro/Summer.java
+++
@@ -1,9 +0,0 @@
-package jini.intro;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Summer extends Remote {
-
-	long sumString(String s) throws InvalidLongException, RemoteException;
-}
