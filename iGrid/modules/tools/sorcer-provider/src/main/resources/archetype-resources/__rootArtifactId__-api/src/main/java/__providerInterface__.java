c3cc15c35900d95204e39fc09dbdcc994ba8154e
rafal.krupinski <rafal.krupinski@5dc8b5b5-2d95-451b-a4db-33bd0ba970db>
2013-04-04 17:04:17 +0000

    make archetype template out of examples/first - WIP
    git-svn-id: https://svn.smtsoftware.com/sorcer/branches/maven@173 5dc8b5b5-2d95-451b-a4db-33bd0ba970db

---
+++ iGrid/modules/tools/sorcer-provider/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/__providerInterface__.java
@@ -0,0 +1,12 @@
+package com.sorcersoft.first;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import sorcer.service.Context;
+
+public interface ${providerInterface} extends Remote {
+	
+	Context sayHelloWorld(Context context) throws RemoteException;
+
+}
