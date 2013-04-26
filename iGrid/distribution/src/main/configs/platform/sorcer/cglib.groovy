41ccde6cdd682b2fe66db3133f904ce1721ec604
pawel.rubach <pawel.rubach@5dc8b5b5-2d95-451b-a4db-33bd0ba970db>
2013-04-26 23:05:03 +0000

    Updated resolver mechanism to read versions.properties from sos-env.jar, updated configs to reflect new resolver mechanism in sos-tests
    git-svn-id: https://svn.smtsoftware.com/sorcer/branches/maven@326 5dc8b5b5-2d95-451b-a4db-33bd0ba970db

---
+++ iGrid/distribution/src/main/configs/platform/sorcer/cglib.groovy
@@ -0,0 +1,10 @@
+import sorcer.provider.boot.SorcerCapabilityDescriptor
+
+def getPlatformCapabilityConfig() {
+    def cap = new SorcerCapabilityDescriptor()
+    cap.name = 'CGLib'
+    cap.version = '2.1_3'
+    cap.setClasspath('cglib:cglib-nodep')
+
+    return cap;
+}
---
+++ iGrid/distribution/src/main/configs/platform/sorcer/sorcer.groovy
@@ -0,0 +1,12 @@
+import sorcer.provider.boot.SorcerCapabilityDescriptor
+import static sorcer.core.SorcerConstants.SORCER_VERSION
+
+def getPlatformCapabilityConfig() {
+    def cap = new SorcerCapabilityDescriptor()
+    cap.name = 'SORCER'
+    cap.version = SORCER_VERSION
+    cap.manufacturer = "SorcerSoft.com"
+    cap.setClasspath(['org.sorcersoft.sorcer:sos-platform', 'org.sorcersoft.sorcer:commons-prv'])
+
+    return cap;
+}
