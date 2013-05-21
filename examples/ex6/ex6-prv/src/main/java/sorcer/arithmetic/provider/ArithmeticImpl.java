d39caf85b299719cdf2310371d306b02193ff875
Michael Sobolewski <sobol@sorcersoft.org>
2013-05-20 22:59:15 -0400

    refactoring examples

---
+++ examples/ex6/ex6-prv/src/main/java/sorcer/arithmetic/provider/ArithmeticImpl.java
@@ -0,0 +1,85 @@
+/**
+ *
+ * Copyright 2013 the original author or authors.
+ * Copyright 2013 Sorcersoft.com S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sorcer.arithmetic.provider;
+
+import java.rmi.RemoteException;
+
+import sorcer.service.Context;
+import sorcer.service.ContextException;
+
+public class ArithmeticImpl implements Arithmetic {
+
+//public class ArithmeticImpl implements Arithmetic, Adder {
+	
+	private Arithmometer arithmometer = new Arithmometer();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see sorcer.ex5.provider.Adder#add(sorcer.service.Context)
+	 */
+	@Override
+	public Context add(Context context) throws RemoteException,
+			ContextException {
+		return arithmometer.add(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * sorcer.ex5.provider.Subtractor#subtract(sorcer.service.Context)
+	 */
+	@Override
+	public Context subtract(Context context) throws RemoteException,
+			ContextException {
+		return arithmometer.subtract(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * sorcer.ex5.provider.Multiplier#multiply(sorcer.service.Context)
+	 */
+	@Override
+	public Context multiply(Context context) throws RemoteException,
+			ContextException {
+		return arithmometer.multiply(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see sorcer.ex5.provider#divide(sorcer.service.Context)
+	 */
+	@Override
+	public Context divide(Context context) throws RemoteException,
+			ContextException {
+		return arithmometer.divide(context);
+	}
+
+    /*
+	 * (non-Javadoc)
+	 *
+	 * @see sorcer.ex5.provider.Averager#average(sorcer.service.Context)
+	 */
+    @Override
+    public Context average(Context context) throws RemoteException, ContextException {
+        return arithmometer.average(context);    }
+}
--- examples/ex6/ex6-prv/src/main/java/sorcer/arithmetic/provider/Arithmometer.java
+++ examples/ex6/ex6-prv/src/main/java/sorcer/arithmetic/provider/Arithmometer.java
@@ -17,6 +17,7 @@
  */
 package sorcer.arithmetic.provider;
 
+import static sorcer.eo.operator.revalue;
 import static sorcer.eo.operator.path;
 
 import java.io.Serializable;
@@ -26,14 +27,17 @@ import java.rmi.RemoteException;
 import java.util.List;
 import java.util.logging.Logger;
 
+import sorcer.core.SorcerConstants;
 import sorcer.core.context.ArrayContext;
+import sorcer.core.context.Contexts;
 import sorcer.core.context.PositionalContext;
 import sorcer.core.context.ServiceContext;
-import sorcer.core.signature.ServiceSignature;
 import sorcer.service.Context;
 import sorcer.service.ContextException;
+import sorcer.service.Signature.ReturnPath;
 
-public class Arithmometer implements Serializable {
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class Arithmometer implements Serializable, SorcerConstants {
 	
 	public static final String ADD = "add";
 
@@ -42,8 +46,10 @@ public class Arithmometer implements Serializable {
 	public static final String MULTIPLY = "multiply";
 
 	public static final String DIVIDE = "divide";
-	
-	public static final String RESULT_PATH = "result/value";
+
+    public static final String AVERAGE = "average";
+
+    public static final String RESULT_PATH = "result/value";
 			
 	public final static Logger logger = Logger.getLogger(Arithmometer.class
 			.getName());
@@ -52,10 +58,10 @@ public class Arithmometer implements Serializable {
 	 * Implements the {@link Adder} interface.
 	 * 
 	 * @param context
-	 *            input dataContext for this operation
-	 * @return an output service dataContext
-	 * @throws RemoteException
-	 * @throws ContextException 
+	 *            input context for this operation
+	 * @return an output service context
+	 * @throws java.rmi.RemoteException
+	 * @throws sorcer.service.ContextException
 	 */
 	public Context add(Context context) throws RemoteException, ContextException {
 		if (context instanceof ArrayContext) {
@@ -63,17 +69,16 @@ public class Arithmometer implements Serializable {
 		} else {
 			return calculateFromPositionalContext(context, ADD);
 		}
-
 	}
 
 	/**
 	 * Implements the {@link Subtractor} interface.
 	 * 
 	 * @param context
-	 *            input dataContext for this operation
-	 * @return an output service dataContext
-	 * @throws RemoteException
-	 * @throws ContextException 
+	 *            input context for this operation
+	 * @return an output service context
+	 * @throws java.rmi.RemoteException
+	 * @throws sorcer.service.ContextException
 	 */
 	public Context subtract(Context context)
 			throws RemoteException, ContextException {
@@ -88,10 +93,10 @@ public class Arithmometer implements Serializable {
 	 * Implements the {@link Multiplier} interface.
 	 * 
 	 * @param context
-	 *            input dataContext for this operation
-	 * @return an output service dataContext
-	 * @throws RemoteException
-	 * @throws ContextException 
+	 *            input context for this operation
+	 * @return an output service context
+	 * @throws java.rmi.RemoteException
+	 * @throws sorcer.service.ContextException
 	 */
 	public Context multiply(Context context)
 			throws RemoteException, ContextException {
@@ -106,10 +111,10 @@ public class Arithmometer implements Serializable {
 	 * Implements the {@link Divider} interface.
 	 * 
 	 * @param context
-	 *            input dataContext for this operation
-	 * @return an output service dataContext
-	 * @throws ContextException 
-	 * @throws RemoteException
+	 *            input context for this operation
+	 * @return an output service context
+	 * @throws sorcer.service.ContextException
+	 * @throws java.rmi.RemoteException
 	 */
 	public Context divide(Context context) throws RemoteException, ContextException {
 		if (context instanceof ArrayContext) {
@@ -119,19 +124,36 @@ public class Arithmometer implements Serializable {
 		}
 	}
 
+    /**
+     * Implements the {@link Divider} interface.
+     *
+     * @param context
+     *            input context for this operation
+     * @return an output service context
+     * @throws sorcer.service.ContextException
+     * @throws java.rmi.RemoteException
+     */
+    public Context average(Context context) throws RemoteException, ContextException {
+        if (context instanceof ArrayContext) {
+            return calculateFromArrayContext(context, AVERAGE);
+        } else {
+            return calculateFromPositionalContext(context, AVERAGE);
+        }
+    }
+
 	/**
 	 * Calculates the result of arithmetic operation specified by a selector
 	 * (add, subtract, multiply, or divide) from the instance of ArrayContext.
 	 * 
 	 * @param context
-	 *            service dataContext
+	 *            service context
 	 * @param selector
 	 *            a name of arithmetic operation
 	 * @return
-	 * @throws RemoteException
-	 * @throws ContextException 
-	 * @throws ContextException
-	 * @throws UnknownHostException
+	 * @throws java.rmi.RemoteException
+	 * @throws sorcer.service.ContextException
+	 * @throws sorcer.service.ContextException
+	 * @throws java.net.UnknownHostException
 	 */
 	private Context calculateFromArrayContext(Context context, String selector)
 			throws RemoteException, ContextException {
@@ -160,10 +182,17 @@ public class Arithmometer implements Serializable {
 				result = inputs.get(0);
 				for (int i = 1; i < inputs.size(); i++)
 					result /= inputs.get(i);
-			}
+			}  else if (selector.equals(AVERAGE)) {
+                result = 0;
+                for (Double value : inputs)
+                    result += value;
+                result = result/inputs.size();
+            } else throw new ContextException("Not supported operation: " + selector);
 
 			logger.info(selector + " result: \n" + result);
+
 			String outputMessage = "calculated by " + getHostname();
+			// set return value
 			if (context.getReturnPath() != null) {
 				context.setReturnValue(result);
 			}
@@ -193,54 +222,74 @@ public class Arithmometer implements Serializable {
 	 * (add, subtract, multiply, or divide) from the instance of ServiceContext.
 	 * 
 	 * @param context
-	 *            service dataContext
+	 *            service context
 	 * @param selector
 	 *            a name of arithmetic operation
 	 * @return
-	 * @throws RemoteException
-	 * @throws ContextException
-	 * @throws UnknownHostException
+	 * @throws java.rmi.RemoteException
+	 * @throws sorcer.service.ContextException
+	 * @throws java.net.UnknownHostException
 	 */
 	private Context calculateFromPositionalContext(Context context, String selector)
 			throws RemoteException, ContextException {
 		PositionalContext cxt = (PositionalContext) context;
 		try {
+			//logger.info("selector: " + ((ServiceContext)context).getCurrentSelector());
 			// get sorted list of input values
-			List<Double> inputs = (List<Double>)cxt.getInValues();
+			List<Double> inputs = (List<Double>)Contexts.getNamedInValues(context);
+			if (inputs == null || inputs.size() == 0) {
+				inputs = (List<Double>)Contexts.getPrefixedInValues(context);
+				logger.info("prefixed inputs: \n" + inputs);
+			}
+			//logger.info("named inputs: \n" + inputs);
+			if (inputs == null || inputs.size() == 0)
+				inputs = (List<Double>)cxt.getInValues();
 			//logger.info("inputs: \n" + inputs);
 			List<String> outpaths = cxt.getOutPaths();
 			//logger.info("outpaths: \n" + outpaths);
 
 			double result = 0.0;
 			if (selector.equals(ADD)) {
-					result = inputs.get(0);
+					result = (Double)revalue(inputs.get(0));
 				for (int i = 1; i < inputs.size(); i++)
-					result += inputs.get(i);
+					result += (Double)revalue(inputs.get(i));
 			} else if (selector.equals(SUBTRACT)) {
-				if (inputs.size() > 2) {
-					throw new ContextException("more than two arguments for subtraction");
+				ReturnPath<?> rp = ((ServiceContext<?>) context).getReturnPath();
+				if (rp != null && rp.argPaths != null && rp.argPaths.length > 0) {
+					result = (Double) revalue(cxt.getValue(rp.argPaths[0]));
+					result -= (Double) revalue(cxt.getValue(rp.argPaths[1]));
+				} else {
+					if (inputs.size() > 2) {
+						throw new ContextException("more than two arguments for subtraction");
+					}
+					result = (Double) revalue(cxt.getInValueAt(1));
+					result -= (Double) revalue(cxt.getInValueAt(2));
 				}
-				result = (Double)cxt.getInValueAt(1);
-				result -= (Double)cxt.getInValueAt(2);
 			} else if (selector.equals(MULTIPLY)) {
 				result = inputs.get(0);
 				for (int i = 1; i < inputs.size(); i++)
-					result *= inputs.get(i);
+					result *= (Double)revalue(inputs.get(i));
 			} else if (selector.equals(DIVIDE)) {
 				if (inputs.size() > 2)
 					throw new ContextException("more than two arguments for division");
-				result = (Double)cxt.getInValueAt(1);
-				result /= (Double)cxt.getInValueAt(2);
-			}
+				result = (Double)revalue(cxt.getInValueAt(1));
+				result /= (Double)revalue(cxt.getInValueAt(2));
+			} else if (selector.equals(AVERAGE)) {
+                result = (Double)revalue(inputs.get(0));
+                for (int i = 1; i < inputs.size(); i++) {
+                    result += (Double)revalue(inputs.get(i));
+                }
+                result = result/inputs.size();
+            } else {
+                throw new ContextException("Not supported operation: " +  selector);
+            }
 
-			//logger.info(selector + " result: \n" + result);
+			logger.info(selector + " result: \n" + result);
 
 			String outputMessage = "calculated by " + getHostname();
-			// set return value
 			if (context.getReturnPath() != null) {
-				context.setReturnValue(result);
+				((ServiceContext)context).setReturnValue(result);
 			}
-			//other ways to indicate the output value
 			else if (outpaths.size() == 1) {
 				// put the result in the existing output path
 				cxt.putValue(outpaths.get(0), result);
@@ -263,7 +312,7 @@ public class Arithmometer implements Serializable {
 	 * Returns name of the local host.
 	 * 
 	 * @return local host name
-	 * @throws UnknownHostException
+	 * @throws java.net.UnknownHostException
 	 */
 	private String getHostname() throws UnknownHostException {
 		return InetAddress.getLocalHost().getHostName();
