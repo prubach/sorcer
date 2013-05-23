dd250ecaf0c000a6087b619baa49cc0ddb8349f5
Paweł Rubach <pawel.rubach@orange.com>
2013-05-23 18:41:00 +0200

    added bat file for prepare-repo, added starting sorcer for CI

---
+++ distribution/src/main/examples/prepare-repository.bat
@@ -0,0 +1,22 @@
+@echo off
+pushd
+IF defined SORCER_HOME ( 
+	set LIB_DIR=%SORCER_HOME%\lib    
+) ELSE (
+	set SORCER_HOME=%CD%\..
+	set LIB_DIR=%CD%\..\lib
+	rem call %SORCER_HOME%\bin\common-run.bat
+)
+
+echo ##############################################################################
+echo ##                       SORCER Examples - prepare repository                                
+echo ##   SORCER_HOME: %SORCER_HOME%
+echo ##   
+echo ##############################################################################
+echo .
+
+set INST_CLASSPATH=%LIB_DIR%\sorcer\sos-env.jar;%LIB_DIR%\sorcer\sos-util.jar;%LIB_DIR%\commons\slf4j-api.jar;%LIB_DIR%\commons\slf4j-simple.jar;%LIB_DIR%\commons\commons-io.jar
+call java -classpath "%INST_CLASSPATH%" sorcer.installer.Installer
+rem Workaround an issue with maven archetype crawl that creates the archetype-catalog.xml file inside the repository instead of the .m2 directory
+rem call mvn archetype:crawl -Dcatalog=%HOMEDRIVE%%HOMEPATH%\.m2\archetype-catalog.xml
+popd
---
+++ src/site/markdown/cont-int.md
@@ -0,0 +1,12 @@
+## Continuous Integration
+
+Sorcer uses the Travic-CI service for continuous integration tests.
+The console is available at:
+
+<pre>https://travis-ci.org/sorcersoft/sorcer</pre>
+
+## Status
+
+The current status of the build is:
+[![Build Status](https://api.travis-ci.org/sorcersoft/sorcer.png)](https://api.travis-ci.org/sorcersoft/sorcer.png)
+
