80b8b48728678aa3ccb6fcdeadede31fcccd1e43
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2013-05-27 15:47:51 +0200

    ignore RepositoryArtifactResolverTest

--- sos/sos-env/src/test/java/sorcer/resolver/RepositoryArtifactResolverTest.java
+++ sos/sos-env/src/test/java/sorcer/resolver/RepositoryArtifactResolverTest.java
@@ -11,6 +11,7 @@ import java.io.File;
  *
  * @author Rafał Krupiński
  */
+@Ignore
 public class RepositoryArtifactResolverTest {
 	RepositoryArtifactResolver resolver = new RepositoryArtifactResolver(new File(System.getProperty("user.home"), ".m2/repository").getPath());
 
