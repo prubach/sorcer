1f582f3e85ad5ec1e6ea4a83d6cfb7d4492fe4ea
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2013-07-24 14:18:06 +0200

    [pom] change version to 1.0-M3-SNAPSHOT

--- sos/sos-env/src/main/java/sorcer/core/SorcerConstants.java
+++ sos/sos-env/src/main/java/sorcer/core/SorcerConstants.java
@@ -30,7 +30,7 @@ public interface SorcerConstants {
     // S_ATTRIBUTE is variable name in System properties
     // E_ATTRIBUTE is variable name in the system Environment
 	/* service provider genetic properties */
-	public static final String SORCER_VERSION = "1.0-M2";
+	public static final String SORCER_VERSION = "1.0-M3-SNAPSHOT";
 	// SORCER global properties defined in sorcer.util.SorcerEnv.java
 	public static final String SORCER_HOME = "sorcer.home";
 
