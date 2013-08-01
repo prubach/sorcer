e507db0e9277bd0530ebf8776709acdc233465e5
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2013-08-01 12:34:21 +0200

    Fix LibraryPathHelper when adding same path more than once

--- sos/sos-util/src/main/java/sorcer/util/LibraryPathHelper.java
+++ sos/sos-util/src/main/java/sorcer/util/LibraryPathHelper.java
@@ -19,22 +19,37 @@ package sorcer.util;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static sorcer.util.JavaSystemProperties.LIBRARY_PATH;
 
 /**
  * @author Rafał Krupiński
  */
 public class LibraryPathHelper {
-	private static final String KEY_LIB_PATH = "java.library.path";
 	private static final Logger log = LoggerFactory.getLogger(LibraryPathHelper.class);
 
 	static Field fieldSysPath;
 	static {
-		try {
-			fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+        fieldSysPath = prepareSysPathField();
+    }
+
+    private static Set<String> initElements() {
+        Set<String> result = new HashSet<String>();
+        Collections.addAll(result, System.getProperty(LIBRARY_PATH,"").split(File.pathSeparator));
+        return result;
+    }
+
+    private static Field prepareSysPathField() {
+        try {
+            Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
 			fieldSysPath.setAccessible(true);
+            return fieldSysPath;
 		} catch (Exception e) {
 			throw new RuntimeException("Could not update java.library.path system property", e);
 		}
@@ -46,14 +61,18 @@ public class LibraryPathHelper {
 			throw new IllegalArgumentException("Could not access directory " + libraryPath);
 		}
 
-		String systemLibPath = System.getProperty(KEY_LIB_PATH);
-        if (systemLibPath == null || systemLibPath.isEmpty()) {
-			systemLibPath = libraryPath;
-		} else {
-			systemLibPath += File.pathSeparator + libraryPath;
-		}
-		log.info("New {} = {}", KEY_LIB_PATH, systemLibPath);
-		System.setProperty(KEY_LIB_PATH, systemLibPath);
+        Set<String> elements = initElements();
+        if(elements.contains(libraryPath)){
+            return;
+        }
+
+        String systemLibPath = StringUtils.join(elements, File.pathSeparator);
+		log.info("New {} = {}", LIBRARY_PATH, systemLibPath);
+        doUpdateLibraryPath(systemLibPath);
+	}
+
+    private static void doUpdateLibraryPath(String libraryPath) {
+        System.setProperty(LIBRARY_PATH, libraryPath);
 
 		try {
 			fieldSysPath.set(null, null);
