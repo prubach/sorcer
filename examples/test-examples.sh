b28a716372527386b253ec07405b0a8a6a04686d
Paweł Rubach <pawel.rubach@sorcersoft.com>
2013-08-27 02:51:35 +0200

    Fixed startup scripts in ex1 to enable working with Rio, modified test-examples script to allow testing with rio

--- examples/test-examples.sh
+++ examples/test-examples.sh
@@ -4,28 +4,33 @@ EX_DIR=$SORCER_HOME/examples
 LOG_DIR=/tmp/logs/
 
 stopSorcer ( ) {
-if [ "$USE_RIO" == "1" ]; then
-  $SORCER_HOME/bin/rio undeploy $SORCER_HOME/configs/SorcerBoot.groovy
-fi
   for p in `jps | grep ServiceStarter | cut -d " " -f 1`; do
-    kill $p > /dev/null
+    kill -9 $p > /dev/null
+    if [ "$USE_RIO" == "1" ]; then
+        if [ -f $SORCER_HOME/logs/all-$p.log ]; then
+            echo "jest log"
+            mv $SORCER_HOME/logs/all-$p.log $1/
+        fi
+    fi
+
   done
 }
 
 startSorcer ( ) {
 if [ "$USE_RIO" == "1" ]; then
   $SORCER_HOME/bin/rio-boot > $1 &
-  sleep 8
-  $SORCER_HOME/bin/rio deploy $SORCER_HOME/configs/SorcerBoot.groovy
+  sleep 12
+  $SORCER_HOME/bin/rio deploy $SORCER_HOME/configs/SorcerBoot.groovy > $1_deploy &
+  sleep 13
 else
   #$SORCER_HOME/bin/sorcer-boot > $1 &
   ant -f $SORCER_HOME/bin/sorcer-boot.xml > $1 &
+  sleep 8
 fi
-sleep 8
 }
 
 restartSorcer ( ) {
-  stopSorcer
+  stopSorcer  $2
   startSorcer $1
 }
 
@@ -44,7 +49,7 @@ cleanLogs ( ) {
 ex0 ( ) {
   EX=ex0
   mkdir $LOG_DIR/$EX
-  restartSorcer $LOG_DIR/$EX/socer-$EX.log
+  restartSorcer $LOG_DIR/$EX/socer-$EX.log $LOG_DIR/$EX/
   if [ "$1" == "rio" ]; then
     $SORCER_HOME/bin/rio deploy $SORCER_HOME/examples/ex0/ex0-prv/Ex0Boot.groovy
   else
@@ -53,8 +58,8 @@ ex0 ( ) {
   fi
   sleep 8
   cd $EX_DIR/$EX/$EX-req/
-  ant -f run.xml > $LOG_DIR/$EX/req.log
-  ./run.ntl >> $LOG_DIR/$EX/req.log
+  ant -f run.xml > $LOG_DIR/$EX/req$1.log
+  ./run.ntl >> $LOG_DIR/$EX/req$1.log
 }
 
 ex1 ( ) {
@@ -63,7 +68,7 @@ ex1 ( ) {
   # thrown on purpose by the second provider (whoIsIt2...)
   EX=ex1
   mkdir $LOG_DIR/$EX
-  restartSorcer $LOG_DIR/$EX/run-bean-socer-$EX.log
+  restartSorcer $LOG_DIR/$EX/run-bean-socer-$EX.log $LOG_DIR/$EX/
   cd $EX_DIR/$EX/run-bean/bin/
   ant -f whoIsItBean1-prv-run.xml > $LOG_DIR/$EX/run-bean-whoIsItBean1-prv.log &
   #ant -f whoIsItBean2-prv-run.xml > $LOG_DIR/$EX/run-bean-whoIsItBean2-prv.log &
@@ -74,7 +79,7 @@ ex1 ( ) {
   ant -f whoIsItBean2-app-run.xml >> $LOG_DIR/$EX/run-bean_req.log
 
   ## ex1 run-prv boot
-  restartSorcer $LOG_DIR/$EX/run-prv-boot-socer-$EX.log
+  restartSorcer $LOG_DIR/$EX/run-prv-boot-socer-$EX.log $LOG_DIR/$EX/
   cd $EX_DIR/$EX/run-prv/bin/
   ant -f whoIsIt-prvs-boot.xml > $LOG_DIR/$EX/run-prv-boot-whoIsIt.log &
   sleep 5
@@ -86,7 +91,7 @@ ex1 ( ) {
   ant -f whoIsItPullJob-app-run.xml >> $LOG_DIR/$EX/run-prv_boot_req.log
 
   ## ex1 run-prv run
-  restartSorcer $LOG_DIR/$EX/run-prv-socer-$EX.log
+  restartSorcer $LOG_DIR/$EX/run-prv-socer-$EX.log $LOG_DIR/$EX/
   cd $EX_DIR/$EX/run-prv/bin/
   ant -f whoIsIt1-prv-run.xml > $LOG_DIR/$EX/run-prv-whoIsIt1.log &
   ant -f whoIsIt2-prv-run.xml > $LOG_DIR/$EX/whoIsIt2-prv.log &
@@ -102,7 +107,7 @@ ex1 ( ) {
 ex2 ( ) {
   EX=ex2
   mkdir $LOG_DIR/$EX
-  restartSorcer $LOG_DIR/$EX/socer-$EX.log
+  restartSorcer $LOG_DIR/$EX/socer-$EX.log $LOG_DIR/$EX/
   cd $EX_DIR/$EX/$EX-prv/
   ant -f worker1-prv-run.xml > $LOG_DIR/$EX/worker1-prv-run.log &
   ant -f worker2-prv-run.xml > $LOG_DIR/$EX/worker2-prv-run.log &
@@ -119,7 +124,7 @@ ex2 ( ) {
 ex3 ( ) {
   EX=ex3
   mkdir $LOG_DIR/$EX
-  restartSorcer $LOG_DIR/$EX/socer-$EX.log
+  restartSorcer $LOG_DIR/$EX/socer-$EX.log $LOG_DIR/$EX/
   cd $EX_DIR/ex2/ex2-prv/
   ant -f worker1-prv-run.xml > $LOG_DIR/$EX/worker1-prv-run.log &
   ant -f worker2-prv-run.xml > $LOG_DIR/$EX/worker2-prv-run.log &
@@ -134,7 +139,7 @@ ex3 ( ) {
 ex4 ( ) {
   EX=ex4
   mkdir $LOG_DIR/$EX
-  restartSorcer $LOG_DIR/$EX/socer-$EX.log
+  restartSorcer $LOG_DIR/$EX/socer-$EX.log $LOG_DIR/$EX/
   cd $EX_DIR/ex2/ex2-prv/
   ant -f worker1-prv-run.xml > $LOG_DIR/$EX/worker1-prv-run.log &
   ant -f worker2-prv-run.xml > $LOG_DIR/$EX/worker2-prv-run.log &
@@ -150,7 +155,7 @@ ex5 ( ) {
   TYPE=$1
   EX=ex5
   mkdir $LOG_DIR/$EX
-  restartSorcer $LOG_DIR/$EX/socer-$EX-$TYPE.log
+  restartSorcer $LOG_DIR/$EX/socer-$EX-$TYPE.log $LOG_DIR/$EX/
   cd $EX_DIR/$EX/$EX-prv/
   ant -f arithmetic-$TYPE.xml > $LOG_DIR/$EX/$TYPE-arithmetic.log &
   cd $EX_DIR/$EX/$EX-req/
@@ -163,7 +168,7 @@ ex6 ( ) {
   TYPE=$1
   EX=ex6
   mkdir $LOG_DIR/$EX
-  restartSorcer $LOG_DIR/$EX/socer-$EX-$TYPE.log
+  restartSorcer $LOG_DIR/$EX/socer-$EX-$TYPE.log $LOG_DIR/$EX/
 
   if [ "$1" == "rio" ]; then
     $SORCER_HOME/bin/rio deploy $SORCER_HOME/examples/ex6/ex6-prv/Ex6Boot.groovy
@@ -176,14 +181,14 @@ ex6 ( ) {
   sleep 8
   mvn -Dmaven.test.skip=false -DskipTests=false test > $LOG_DIR/$EX/$TYPE-req.log 
   ant -f f5-req-run.xml > $LOG_DIR/$EX/$TYPE-f5-req.log
-  ant -f f5a-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5-req.log
-  ant -f f5m-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5-req.log
-  ant -f f5pull-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5-req.log
-  ant -f f5xP-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5-req.log
-  ant -f f5xS-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5-req.log
+  ant -f f5a-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5a-req.log
+  ant -f f5m-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5m-req.log
+  ant -f f5pull-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5pull-req.log
+  ant -f f5xP-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5xP-req.log
+  ant -f f5xS-req-run.xml >> $LOG_DIR/$EX/$TYPE-f5xS-req.log
   ant -f f1-req-run.xml > $LOG_DIR/$EX/$TYPE-f1-req.log
-  ant -f f1-PAR-pull-run.xml >> $LOG_DIR/$EX/$TYPE-f1-req.log
-  ant -f f1-SEQ-pull-run.xml >> $LOG_DIR/$EX/$TYPE-f1-req.log
+  ant -f f1-PAR-pull-run.xml >> $LOG_DIR/$EX/$TYPE-f1-PAR-req.log
+  ant -f f1-SEQ-pull-run.xml >> $LOG_DIR/$EX/$TYPE-f1-SEQ-req.log
   nsh f1.ntl > $LOG_DIR/$EX/$TYPE-ntl-req.log    
 }
 
@@ -201,7 +206,7 @@ ex0
 ex0 rio
 #ex1
 #ex2
-#ex3
+ex3
 #ex4
 #ex5 all-beans-boot
 #ex5 all-prv-boot
@@ -209,8 +214,7 @@ ex0 rio
 #ex6 all-beans-boot
 #ex6 all-prvs-run
 #ex6 prv-boot
-
-ex6 rio
+#ex6 rio
 
 stopSorcer
 showExceptions
