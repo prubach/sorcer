6e615f2ec27d97a3928a9ae88f13cb1e7d13f034
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2013-09-26 15:20:22 +0200

    format SorcerBoot.groovy

--- configs/SorcerBoot.groovy
+++ configs/SorcerBoot.groovy
@@ -26,7 +26,7 @@ def getSorcerVersion() {
 
 
 def String getCodebase() {
-    return 'http://'+SorcerEnv.getLocalHost().getHostAddress()+":9010"
+    return 'http://' + SorcerEnv.getLocalHost().getHostAddress() + ":9010"
 }
 
 deployment(name: 'Sorcer') {
@@ -34,55 +34,55 @@ deployment(name: 'Sorcer') {
 
     codebase getCodebase()
 
-    artifact id:'mahalo', 'org.apache.river:mahalo:2.2.1'
-    artifact id:'mahalo-dl', 'org.apache.river:mahalo-dl:2.2.1'
-    artifact id:'sos-exertlet-sui', "org.sorcersoft.sorcer:sos-exertlet-sui:"+getSorcerVersion()
-    artifact id:'cataloger-prv', "org.sorcersoft.sorcer:cataloger-prv:"+getSorcerVersion()
-    artifact id:'cataloger-api', "org.sorcersoft.sorcer:cataloger-api:"+getSorcerVersion()
-    artifact id:'jobber-prv', "org.sorcersoft.sorcer:jobber-prv:"+getSorcerVersion()
-    artifact id:'spacer-prv', "org.sorcersoft.sorcer:spacer-prv:"+getSorcerVersion()
-    artifact id:'logger-prv', "org.sorcersoft.sorcer:logger-prv:"+getSorcerVersion()
-    artifact id:'logger-sui', "org.sorcersoft.sorcer:logger-sui:"+getSorcerVersion()
-    artifact id:'dbp-prv', "org.sorcersoft.sorcer:dbp-prv:"+getSorcerVersion()
-    artifact id:'exertmonitor-prv', "org.sorcersoft.sorcer:exertmonitor-prv:"+getSorcerVersion()
-    artifact id:'commons-prv', "org.sorcersoft.sorcer:commons-prv:"+getSorcerVersion()
+    artifact id: 'mahalo', 'org.apache.river:mahalo:2.2.1'
+    artifact id: 'mahalo-dl', 'org.apache.river:mahalo-dl:2.2.1'
+    artifact id: 'sos-exertlet-sui', "org.sorcersoft.sorcer:sos-exertlet-sui:" + getSorcerVersion()
+    artifact id: 'cataloger-prv', "org.sorcersoft.sorcer:cataloger-prv:" + getSorcerVersion()
+    artifact id: 'cataloger-api', "org.sorcersoft.sorcer:cataloger-api:" + getSorcerVersion()
+    artifact id: 'jobber-prv', "org.sorcersoft.sorcer:jobber-prv:" + getSorcerVersion()
+    artifact id: 'spacer-prv', "org.sorcersoft.sorcer:spacer-prv:" + getSorcerVersion()
+    artifact id: 'logger-prv', "org.sorcersoft.sorcer:logger-prv:" + getSorcerVersion()
+    artifact id: 'logger-sui', "org.sorcersoft.sorcer:logger-sui:" + getSorcerVersion()
+    artifact id: 'dbp-prv', "org.sorcersoft.sorcer:dbp-prv:" + getSorcerVersion()
+    artifact id: 'exertmonitor-prv', "org.sorcersoft.sorcer:exertmonitor-prv:" + getSorcerVersion()
+    artifact id: 'commons-prv', "org.sorcersoft.sorcer:commons-prv:" + getSorcerVersion()
 
     artifact id: 'blitz-dl', 'org.sorcersoft.blitz:blitz-proxy:2.2.0'
     artifact id: 'blitz-impl', 'org.sorcersoft.blitz:blitz-service:2.2.0'
 
-    service(name:'Mahalo') {
-         interfaces {
-             classes 'net.jini.core.transaction.server.TransactionManager'
-             artifact ref:'mahalo-dl'
-         }
-         implementation(class: 'com.sun.jini.mahalo.TransientMahaloImpl') {
-             artifact ref:'mahalo'
-         }
-         configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}jini${fs()}configs${fs()}mahalo.config"
-         maintain 1
-     }
+    service(name: 'Mahalo') {
+        interfaces {
+            classes 'net.jini.core.transaction.server.TransactionManager'
+            artifact ref: 'mahalo-dl'
+        }
+        implementation(class: 'com.sun.jini.mahalo.TransientMahaloImpl') {
+            artifact ref: 'mahalo'
+        }
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}jini${fs()}configs${fs()}mahalo.config"
+        maintain 1
+    }
 
     service(name: "BlitzSpace") {
         interfaces {
             classes 'net.jini.space.JavaSpace05'
-            artifact ref:'blitz-dl'
+            artifact ref: 'blitz-dl'
         }
         implementation(class: 'org.dancres.blitz.remote.BlitzServiceImpl') {
-            artifact ref:'blitz-impl'
+            artifact ref: 'blitz-impl'
         }
-        configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}blitz${fs()}configs${fs()}blitz.config"
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}blitz${fs()}configs${fs()}blitz.config"
         maintain 1
     }
 
     service(name: "Jobber") {
         interfaces {
             classes 'sorcer.core.provider.Jobber'
-            artifact ref:'sos-exertlet-sui'
+            artifact ref: 'sos-exertlet-sui'
         }
         implementation(class: 'sorcer.core.provider.jobber.ServiceJobber') {
             artifact ref: 'jobber-prv'
         }
-        configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}sos-providers${fs()}jobber.config"
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}sos-providers${fs()}jobber.config"
         maintain 1
     }
 
@@ -94,11 +94,11 @@ deployment(name: 'Sorcer') {
         implementation(class: 'sorcer.core.provider.jobber.ServiceSpacer') {
             artifact ref: 'spacer-prv'
         }
-        configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}sos-providers${fs()}spacer.config"
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}sos-providers${fs()}spacer.config"
         maintain 1
     }
 
-      service(name: "Cataloger") {
+    service(name: "Cataloger") {
         interfaces {
             classes 'sorcer.core.provider.Cataloger'
             artifact ref: 'cataloger-api'
@@ -106,7 +106,7 @@ deployment(name: 'Sorcer') {
         implementation(class: 'sorcer.core.provider.cataloger.ServiceCataloger') {
             artifact ref: 'cataloger-prv'
         }
-        configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}sos-providers${fs()}cataloger.config"
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}sos-providers${fs()}cataloger.config"
         maintain 1
     }
 
@@ -118,7 +118,7 @@ deployment(name: 'Sorcer') {
         implementation(class: 'sorcer.core.provider.logger.RemoteLoggerManager') {
             artifact ref: 'logger-prv'
         }
-        configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}sos-providers${fs()}logger.config"
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}sos-providers${fs()}logger.config"
         maintain 1
     }
 
@@ -130,7 +130,7 @@ deployment(name: 'Sorcer') {
         implementation(class: 'sorcer.core.provider.exertmonitor.ExertMonitor') {
             artifact ref: 'exertmonitor-prv'
         }
-        configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}sos-providers${fs()}exertmonitor.config"
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}sos-providers${fs()}exertmonitor.config"
         maintain 1
     }
 
@@ -142,7 +142,7 @@ deployment(name: 'Sorcer') {
         implementation(class: 'sorcer.core.provider.dbp.DatabaseProvider') {
             artifact ref: 'dbp-prv'
         }
-        configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}sos-providers${fs()}dbp.config"
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}sos-providers${fs()}dbp.config"
         maintain 1
     }
 
@@ -154,7 +154,7 @@ deployment(name: 'Sorcer') {
         implementation(class: 'sorcer.core.provider.ServiceTasker') {
             artifact ref: 'commons-prv'
         }
-        configuration file: ""+getSorcerHome()+"${fs()}configs${fs()}sos-providers${fs()}exerter.config"
+        configuration file: "" + getSorcerHome() + "${fs()}configs${fs()}sos-providers${fs()}exerter.config"
         maintain 1
     }
 }
