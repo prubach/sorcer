3625c6d3fe1540965f10ecd4d80a201ee13243f6
Paweł Rubach <pawel.rubach@sorcersoft.com>
2013-10-05 16:41:45 +0200

    Added opstrings to deploy - required to test in dev

---
+++ deploy/Ex1WhoIsItBean1Boot.groovy
@@ -0,0 +1,45 @@
+/**
+ * Deployment configuration for ex1-bean
+ *
+ * @author Pawel Rubach
+ */
+import sorcer.core.SorcerEnv;
+
+String[] getInitialMemberGroups() {
+    def groups = SorcerEnv.getLookupGroups();
+    return groups as String[]
+}
+
+def getSorcerHome() {
+    return sorcerHome = SorcerEnv.getHomeDir();
+}
+
+def getSorcerVersion() {
+    return sorcerVersion = SorcerEnv.getSorcerVersion();
+}
+
+def String getCodebase() {
+    return 'http://'+SorcerEnv.getLocalHost().getHostAddress()+":9010"
+}
+
+
+deployment(name: 'ex1-whoIsIt-1') {
+    groups getInitialMemberGroups();
+
+    codebase getCodebase()
+
+    artifact id:'ex1-api', 'org.sorcersoft.sorcer:ex1-api:'+getSorcerVersion()
+    artifact id:'ex1-prv', 'org.sorcersoft.sorcer:ex1-prv:'+getSorcerVersion()
+
+    service(name:'ex1-whoIsItBean-1') {
+         interfaces {
+             classes 'sorcer.ex1.WhoIsIt'
+             artifact ref:'ex1-api'
+         }
+         implementation(class: 'sorcer.core.provider.ServiceTasker') {
+             artifact ref:'ex1-prv'
+         }
+         configuration file: "${getSorcerHome()}/examples/ex1/ex1-prv/src/main/resources/config/whoIsIt1-prv.config"
+         maintain 1
+     }
+}
---
+++ deploy/Ex5DividerBoot.groovy
@@ -0,0 +1,45 @@
+/**
+ * Deployment configuration for ex5-prv
+ *
+ * @author Pawel Rubach
+ */
+import sorcer.core.SorcerEnv;
+
+String[] getInitialMemberGroups() {
+    def groups = SorcerEnv.getLookupGroups();
+    return groups as String[]
+}
+
+def getSorcerHome() {
+    return sorcerHome = SorcerEnv.getHomeDir();
+}
+
+def getSorcerVersion() {
+    return sorcerVersion = SorcerEnv.getSorcerVersion();
+}
+
+def String getCodebase() {
+    return 'http://'+SorcerEnv.getLocalHost().getHostAddress()+":9010"
+}
+
+
+deployment(name: 'ex5-divider') {
+    groups getInitialMemberGroups();
+
+    codebase getCodebase()
+
+    artifact id:'ex5-api', 'org.sorcersoft.sorcer:ex5-api:'+getSorcerVersion()
+    artifact id:'ex5-prv', 'org.sorcersoft.sorcer:ex5-prv:'+getSorcerVersion()
+
+    service(name:'ex5-divider-prv') {
+         interfaces {
+             classes 'sorcer.ex5.provider.Divider'
+             artifact ref:'ex5-api'
+         }
+         implementation(class: 'sorcer.core.provider.ServiceTasker') {
+             artifact ref:'ex5-prv'
+         }
+         configuration file: "${getSorcerHome()}/examples/ex5/ex5-prv/src/main/resources/config/divider-prv.config"
+         maintain 1
+     }
+}
