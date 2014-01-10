97622ef87a20b2d4c0a218aab8439893388549cc
Rafał Krupiński <rafal@krupinski.info.pl>
2014-01-10 17:16:12 +0100

    change sorcer version to 1.0-M3

--- sos/sos-env/src/main/java/sorcer/core/SorcerConstants.java
+++ sos/sos-env/src/main/java/sorcer/core/SorcerConstants.java
@@ -30,7 +30,7 @@ public interface SorcerConstants {
     // S_ATTRIBUTE is variable name in System properties
     // E_ATTRIBUTE is variable name in the system Environment
 	/* service provider genetic properties */
-    public static final String SORCER_VERSION = "1.0-M3-SNAPSHOT";
+    public static final String SORCER_VERSION = "1.0-M3";
     // Rio default version
     public static final String RIO_VERSION = "5.0-M4-S4";
 	// SORCER global properties defined in sorcer.util.SorcerEnv.java
