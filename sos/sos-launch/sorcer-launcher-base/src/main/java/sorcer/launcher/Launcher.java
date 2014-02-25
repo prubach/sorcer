d5ec0d024894a30aa7ab355a871b906c6ab78a3f
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2014-02-25 18:13:39 +0100

    resolve artifact codebase using custom URIClassLoader - WIP, working

--- sos/sos-launch/sorcer-launcher-base/src/main/java/sorcer/launcher/Launcher.java
+++ sos/sos-launch/sorcer-launcher-base/src/main/java/sorcer/launcher/Launcher.java
@@ -166,7 +166,8 @@ public abstract class Launcher implements ILauncher {
         Map<String, String> sysProps = new HashMap<String, String>();
         sysProps.put(MAX_DATAGRAM_SOCKETS, "1024");
         sysProps.put(PROTOCOL_HANDLER_PKGS, "net.jini.url|sorcer.util.bdb|org.rioproject.url");
-        sysProps.put(RMI_SERVER_CLASS_LOADER, "org.rioproject.rmi.ResolvingLoader");
+//        sysProps.put(RMI_SERVER_CLASS_LOADER, "org.rioproject.rmi.ResolvingLoader");
+        sysProps.put(RMI_SERVER_CLASS_LOADER, "sorcer.rmi.ArtifactRmiLoader");
         sysProps.put(RMI_SERVER_USE_CODEBASE_ONLY, Boolean.FALSE.toString());
 
         sysProps.put(SECURITY_POLICY, policyPath.getPath());
---
+++ tools/sos-boot/src/main/java/sorcer/rmi/ArtifactClassLoader.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014 Sorcersoft.com S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sorcer.rmi;
+
+import edu.emory.mathcs.util.classloader.URIClassLoader;
+import org.rioproject.resolver.Resolver;
+import org.rioproject.resolver.ResolverException;
+import org.rioproject.url.artifact.ArtifactURLConfiguration;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author Rafał Krupiński
+ */
+public class ArtifactClassLoader extends URIClassLoader {
+    private Resolver resolver;
+
+    public ArtifactClassLoader(ClassLoader parent, Resolver resolver) {
+        super(new URI[0], parent);
+        this.resolver = resolver;
+    }
+
+    public void addURI(URI artifact) {
+        if (!"artifact".equals(artifact.getScheme())) {
+            super.addURI(artifact);
+            return;
+        }
+        try {
+            URI[] resolved = resolve(artifact, resolver);
+            for (URI uri : resolved)
+                super.addURI(uri);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        } catch (ResolverException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static URI[] resolve(URI artifactUri, Resolver resolver) throws URISyntaxException, ResolverException {
+        String input = artifactUri.toString();
+        /*String path = artifactUri.getPath();
+        if (path == null) {
+            throw new URISyntaxException(input, "URI has null path");
+        }*/
+
+        ArtifactURLConfiguration configuration = new ArtifactURLConfiguration(input);
+        String artifact = configuration.getArtifact();
+        String A = "artifact:";
+        if (artifact.startsWith(A))
+            artifact = artifact.substring(A.length());
+
+        String[] cp = resolver.getClassPathFor(artifact, configuration.getRepositories());
+        URI[] result = new URI[cp.length];
+        for (int i = 0; i < cp.length; i++)
+            result[i] = new File(cp[i]).toURI();
+        return result;
+    }
+}
