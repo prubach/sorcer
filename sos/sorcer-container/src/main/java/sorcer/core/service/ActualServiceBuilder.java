3b99c21f48a60a53e634e80c6ccb4ca810edc933
Rafał Krupiński <rafal@krupinski.info.pl>
2014-04-12 04:34:44 +0200

    add cglib mixins

--- sos/sorcer-container/src/main/java/sorcer/core/service/ActualServiceBuilder.java
+++ sos/sorcer-container/src/main/java/sorcer/core/service/ActualServiceBuilder.java
@@ -8,35 +8,46 @@ import net.jini.config.ConfigurationException;
 import net.jini.config.ConfigurationProvider;
 import net.jini.config.EmptyConfiguration;
 import net.jini.core.entry.Entry;
+import net.sf.cglib.proxy.Mixin;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sorcer.config.BeanListener;
 import sorcer.core.provider.Provider;
+import sorcer.util.ClassLoaders;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Rafał Krupiński
  */
 public class ActualServiceBuilder<T> implements IServiceBuilder<T> {
-    private Logger log = LoggerFactory.getLogger(ActualServiceBuilder.class);
+    private static Logger log = LoggerFactory.getLogger(ActualServiceBuilder.class);
 
     private Set<Module> modules = new HashSet<Module>();
 
-    private Map<Class, Object> beanMap = new HashMap<Class, Object>();
+    private Map<Class, Object> beanMap = new LinkedHashMap<Class, Object>();
 
     private IServiceBeanListener beanListener;
+
+    private String typeName;
+
+    private ClassLoader serviceClassLoader = ClassLoaders.current();
 
     // service processor specific configurations
     protected final Map<Object, Object> configurations = new HashMap<Object, Object>();
 
     protected Configuration configuration = EmptyConfiguration.INSTANCE;
+
+    public ActualServiceBuilder() {
+    }
+
+    public ActualServiceBuilder(String typeName) {
+        this.typeName = typeName;
+    }
+
 
     @Override
     public void addInterceptor(final MethodInterceptor interceptor) {
@@ -103,6 +114,49 @@ public class ActualServiceBuilder<T> implements IServiceBuilder<T> {
 
     @Override
     public T get() {
-        return null;
+        try {
+            return doGet();
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Could not create bean of type " + typeName, e);
+        } catch (InstantiationException e) {
+            throw new IllegalStateException("Could not create bean of type " + typeName, e);
+        } catch (IllegalAccessException e) {
+            throw new IllegalStateException("Could not create bean of type " + typeName, e);
+        }
+    }
+
+    private T doGet() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Map<Class, Object> beans = new HashMap<Class, Object>(beanMap);
+        Class<T> type = (Class<T>) Class.forName(typeName, false, serviceClassLoader);
+        T bean = type.newInstance();
+        traverseClass(bean, type, beans);
+        Mixin.Generator generator = new Mixin.Generator();
+        generator.setClassLoader(serviceClassLoader);
+        Set<Class> allTypes = beans.keySet();
+        generator.setClasses(allTypes.toArray(new Class[allTypes.size()]));
+        generator.setDelegates(beans.values().toArray());
+
+        return (T) generator.create();
+    }
+
+    private static void traverseClass(Object o, Class type, Map<Class, Object> result) {
+        Class c = type;
+        do
+            for (Class iface : c.getInterfaces()) {
+                traverseIface(o, iface, result);
+            }
+        while ((c = c.getSuperclass()) != null);
+    }
+
+    private static void traverseIface(Object o, Class type, Map<Class, Object> result) {
+        if (result.containsKey(type)) {
+            Object override = result.get(type);
+            log.info("Interface {} of bean {} is overiden by {}", type, o, override);
+        } else {
+            result.put(type, o);
+        }
+
+        for (Class st : type.getInterfaces())
+            traverseIface(o, st, result);
     }
 }
