30ddd1b41518f3e64859e892d963d81bf25ce8ce
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2014-04-18 13:49:56 +0200

    remove cglib mixin from ServiceBuilder

--- sos/sorcer-container/src/main/java/sorcer/core/service/ActualServiceBuilder.java
+++ sos/sorcer-container/src/main/java/sorcer/core/service/ActualServiceBuilder.java
@@ -8,27 +8,18 @@ import net.jini.config.ConfigurationException;
 import net.jini.config.ConfigurationProvider;
 import net.jini.config.EmptyConfiguration;
 import net.jini.core.entry.Entry;
-import net.jini.core.lookup.ServiceID;
-import net.jini.core.transaction.Transaction;
-import net.jini.core.transaction.TransactionException;
 import net.jini.lookup.entry.Name;
-import net.sf.cglib.proxy.Mixin;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sorcer.config.BeanListener;
-import sorcer.core.UEID;
 import sorcer.core.provider.Provider;
 import sorcer.core.proxy.ProviderProxy;
-import sorcer.jini.lookup.entry.SorcerServiceInfo;
-import sorcer.service.*;
 import sorcer.util.ClassLoaders;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import javax.security.auth.Subject;
 import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.rmi.server.ExportException;
 import java.util.*;
 
@@ -107,7 +98,6 @@ public class ActualServiceBuilder<T> implements IServiceBuilder<T> {
         bean = get();
         beanListener.preProcess(this, bean);
 
-
         if (builderConfig.export) {
             log.debug("Exporting {}", bean);
             proxy = builderConfig.exporter.export((Remote) bean);
@@ -141,12 +131,12 @@ public class ActualServiceBuilder<T> implements IServiceBuilder<T> {
 
     @Override
     public String getName() {
-        return "TODO-UNKNOWN";
+        return builderConfig.type.getSimpleName();
     }
 
     @Override
     public Class<T> getType() {
-        return null;
+        return builderConfig.type;
     }
 
     @Override
@@ -157,7 +147,6 @@ public class ActualServiceBuilder<T> implements IServiceBuilder<T> {
     @Override
     public Entry[] getAttributes() {
         return new Entry[]{
-                //new SorcerServiceInfo(),
                 new Name(getName())
         };
     }
@@ -165,150 +154,11 @@ public class ActualServiceBuilder<T> implements IServiceBuilder<T> {
     @Override
     public T get() {
         try {
-            return doGet();
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Could not create bean of type " + builderConfig.type, e);
+            return (T) builderConfig.type.newInstance();
         } catch (InstantiationException e) {
             throw new IllegalStateException("Could not create bean of type " + builderConfig.type, e);
         } catch (IllegalAccessException e) {
             throw new IllegalStateException("Could not create bean of type " + builderConfig.type, e);
         }
-    }
-
-    private T doGet() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Map<Class, Object> beans = new HashMap<Class, Object>(beanMap);
-        T bean = (T) builderConfig.type.newInstance();
-
-        beans.put(Provider.class, new ProviderImpl(jiniConfig, proxy));
-
-        traverseClass(bean, builderConfig.type, beans);
-        Mixin.Generator generator = new Mixin.Generator();
-        generator.setClassLoader(serviceClassLoader);
-        Set<Class> allTypes = beans.keySet();
-        generator.setClasses(allTypes.toArray(new Class[allTypes.size()]));
-        generator.setDelegates(beans.values().toArray());
-
-        return (T) generator.create();
-    }
-
-    private static void traverseClass(Object o, Class type, Map<Class, Object> result) {
-        Class c = type;
-        do
-            for (Class iface : c.getInterfaces()) {
-                traverseIface(o, iface, result);
-            }
-        while ((c = c.getSuperclass()) != null);
-    }
-
-    private static void traverseIface(Object o, Class type, Map<Class, Object> result) {
-        if (result.containsKey(type)) {
-            Object override = result.get(type);
-            log.info("Interface {} of bean {} is overiden by {}", type, o, override);
-        } else {
-            result.put(type, o);
-        }
-
-        for (Class st : type.getInterfaces())
-            traverseIface(o, st, result);
-    }
-}
-
-class ProviderImpl implements Provider {
-    private Configuration configuration;
-    private Object proxy;
-
-    ProviderImpl(Configuration configuration, Object proxy) {
-        this.configuration = configuration;
-        this.proxy = proxy;
-    }
-
-    @Override
-    public ServiceID getProviderID() throws RemoteException {
-        return new ServiceID(1,2);
-    }
-
-    @Override
-    public String getProviderName() throws RemoteException {
-        return "provider-name";
-    }
-
-    @Override
-    public List<Object> getProperties() throws RemoteException {
-        return new ArrayList<Object>(Arrays.asList("getProperties"));
-    }
-
-    @Override
-    public Configuration getProviderConfiguration() throws RemoteException {
-        return configuration;
-    }
-
-    @Override
-    public boolean mutualExclusion() throws RemoteException {
-        return false;
-    }
-
-    @Override
-    public String getDescription() throws RemoteException {
-        return "description";
-    }
-
-    @Override
-    public boolean isBusy() throws RemoteException {
-        return false;
-    }
-
-    @Override
-    public void destroy() throws RemoteException {
-
-    }
-
-    @Override
-    public Properties getJavaSystemProperties() throws RemoteException {
-        return System.getProperties();
-    }
-
-    @Override
-    public Object getProxy() throws RemoteException {
-        return proxy;
-    }
-
-    @Override
-    public boolean isContextValid(Context<?> dataContext, Signature forSignature) throws RemoteException {
-        return true;
-    }
-
-    @Override
-    public java.util.logging.Logger getLogger() throws RemoteException {
-        return null;
-    }
-
-    @Override
-    public Object getAdmin() throws RemoteException {
-        return proxy;
-    }
-
-    @Override
-    public void stop(UEID ref, Subject subject) throws RemoteException, UnknownExertionException, AccessDeniedException {
-
-    }
-
-    @Override
-    public void suspend(UEID ref, Subject subject) throws RemoteException, UnknownExertionException, AccessDeniedException {
-
-    }
-
-    @Override
-    public void resume(Exertion ex) throws RemoteException, ExertionException {
-
-    }
-
-    @Override
-    public void step(Exertion ex) throws RemoteException, ExertionException {
-
-    }
-
-    @Override
-    public Exertion service(Exertion exertion, Transaction txn) throws TransactionException, ExertionException, RemoteException {
-        return null;
     }
 }
